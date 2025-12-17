094e324947863e6ab37941869b3ecd00862257dd
RANDALL TAN CHUAN HAO 2404986H <2404986H@student.tp.edu.sg>
2025-12-17 22:58:50 +0800

    Added Profile creation for new users

--- DatingApp/Domain/Profile.cs
+++ DatingApp/Domain/Profile.cs
@@ -10,7 +10,7 @@
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
 
-        public int Age { get; set; }
+        public int? Age { get; set; }
         public string? Gender { get; set; }
 
         public string? Bio { get; set; }
